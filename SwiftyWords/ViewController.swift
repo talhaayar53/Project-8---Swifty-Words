e7a0ed29ddc94ed75d0048db6ddc002bdbaeda84
Talha <talhaayar53@gmail.com>
2019-08-18 14:30:47 +0300

    Initial Commit

---
+++ SwiftyWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyWords
+//
+//  Created by TALHA AYAR on 18.08.2019.
+//  Copyright © 2019 Talha Ayar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
